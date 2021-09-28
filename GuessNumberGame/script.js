ae936aa80ecf1629a6787626e5090724861973a5
HiddenIsSquid <amirhaini@gmail.com>
2021-09-29 07:30:16 +0800

    rev 1

--- GuessNumberGame/script.js
+++ GuessNumberGame/script.js
@@ -21,7 +21,7 @@ console.log(document.querySelector('.guess').value);
 
 // document.querySelector('.score').textContent = 14;
 
-const secretNumber = Math.round(Math.random()*20);
+let secretNumber = Math.round(Math.random()*20) + 1;
 
 let score = 20;
 
@@ -40,13 +40,25 @@ document.querySelector('.check').addEventListener('click',function () {
 
 
    
-
+    //error
     if(guess <= 0 || guess>=21) {document.querySelector('.message').textContent = '1 to 20';}
 
+    //player wins
     else if (guess == secretNumber) {
         document.querySelector('.message').textContent = 'Spot on!';
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        document.querySelector('.number').textContent = secretNumber;
+
+        if (score > highscore){
+            document.querySelector('.highscore').textContent = score;
+        }
     }
 
+    //guess is too high
     else if (guess > secretNumber) {
         if (score > 1){
         document.querySelector('.message').textContent = 'lower';
@@ -56,6 +68,7 @@ document.querySelector('.check').addEventListener('click',function () {
         document.querySelector('.message').textContent ="you lose"
         }
     }
+    //guess too low
     else if (guess < secretNumber){
         if (score > 1){
         document.querySelector('.message').textContent = 'higher';
@@ -68,4 +81,33 @@ document.querySelector('.check').addEventListener('click',function () {
     document.querySelector('.score').textContent = score;
 });
 
+
+
+//Implement functionality reset
+
+document.querySelector('.again').addEventListener('click', function () {
+    let score = 20;
+
+    secretNumber = Math.round(Math.random()*20) + 1;
+
+    document.querySelector('.score').textContent = '20'
+
+
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('.message').textContent = 'Start guessing..'
+
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('body').style.backgroundColor ='#222'
+
+    document.querySelector('.number').style.width = '15rem';
+
+    console.log(secretNumber); 
+    
+})
+
+//Implementing highscore
+
+let highscore = 0;
 
